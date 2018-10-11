732ce5d61c0fa5e9d132952d388cf09a284735bf
Bret Stateham <Bret.Stateham@microsoft.com>
2018-10-11 12:59:29 -0700

    Initial map implementation

--- restAPI/MapApp/src/app/app.module.ts
+++ restAPI/MapApp/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { UiModule } from './ui/ui.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    HttpClientModule,
+    BrowserModule,
+    UiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
