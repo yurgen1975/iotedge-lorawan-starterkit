69357bae0a81135b6fe6cf72a3d9be91a3a2e75a
Isabel Cabezas Martín <isacabe@microsoft.com>
2018-10-11 16:02:12 -0700

    DecoderSearchAndRescueGpsSensor method for Seeeduino LoRaWAN W/GPS

--- LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaWan.NetworkServer/LoraDecoders.cs
+++ LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaWan.NetworkServer/LoraDecoders.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using Newtonsoft.Json.Linq;
 
 namespace LoRaWan.NetworkServer
 {
@@ -49,6 +50,41 @@ namespace LoRaWan.NetworkServer
         {
             var result = Encoding.ASCII.GetString(payload);
             return String.Format("{{\"value\": {0}}}", result);
+
+        }
+
+        private static string DecoderSearchAndRescueGpsSensor(byte[] payload, uint fport)
+        {
+            dynamic decoded = new JObject();
+
+
+            if (payload.Length == 2)    /* receiving ONLY ONE delta */
+            {
+                var lat_val = (float)payload[0] - 127; //positive or negative?
+                var lng_val = (float)payload[1] - 127;
+
+                decoded.lat = lat_val / 10000;
+                decoded.lng = lng_val / 10000;
+            }
+            else if (payload.Length == 8)     /* receiving full coordinates */
+            {
+
+                byte[] bytes_lat = { payload[0], payload[1], payload[2], payload[3] };
+                byte[] bytes_lng = { payload[4], payload[5], payload[6], payload[7] };
+
+                //default lora mote
+                var lat_val = BitConverter.ToInt32(bytes_lat, 0);
+                var lng_val = BitConverter.ToInt32(bytes_lng, 0);
+
+                decoded.lat = (float)lat_val / Math.Pow(2, 23) * 90;
+                decoded.lng = (float)lng_val / Math.Pow(2, 23) * 180;
+            }
+            else
+            {
+                return $"{{\"error\": \" length of '{payload.Length}' bytes is not supported, try 8 bytes for a complete GPS coordinate or 2 for a delta }}";
+            }
+
+            return decoded.ToString(Newtonsoft.Json.Formatting.None);
 
         }
     }
